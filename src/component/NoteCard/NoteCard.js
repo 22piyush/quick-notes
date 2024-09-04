6a4f5370d666200ad2216d0d9a117ec2b8929ad6
22piyush <piyushaglawe05@gmail.com>
2024-09-04 09:35:30 +0530

    done

---
+++ src/component/NoteCard/NoteCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NoteCard.css";
+
+function NoteCard({ title, description, category, emoji }) {
+  return (
+    <div className="note-card">
+      <div className="note-card-emoji">{emoji}</div>
+      <div>
+        <h3 className="note-card-title">{title}</h3>
+        <p className="note-card-description">{description}</p>
+        <span className="note-card-category">{category}</span>
+      </div>
+    </div>
+  );
+}
+
+export default NoteCard;
--- src/views/Show/Show.js
+++ src/views/Show/Show.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import "./Show.css"
-import HomeButton from '../../component/HomeButton/HomeButton'
+import React, { useEffect, useState } from "react";
+import "./Show.css";
+import HomeButton from "../../component/HomeButton/HomeButton";
+import NoteCard from "../../component/NoteCard/NoteCard";
 
 function Show() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+    setNotes(savedNotes);
+  }, []);
+
   return (
     <div>
-      <h1 className='text-center text-primary'>📖Show Notes</h1>
-      <HomeButton/>
+      <h1 className="text-center text-primary">📖Show Notes</h1>
+      {
+        notes.map((note)=>{
+          const {title, description, category, emoji} = note;
+
+          return(
+            <div>
+              <NoteCard
+              title={title}
+              description={description}
+              category={category}
+              emoji={emoji}/>
+            </div>
+          )
+        })
+      }
+      <HomeButton />
     </div>
-  )
+  );
 }
 
-export default Show
+export default Show;
