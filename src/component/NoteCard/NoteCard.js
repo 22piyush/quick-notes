f08cd33c1745d22ea9eafdb08e7c501ad61f1584
22piyush <piyushaglawe05@gmail.com>
2024-10-21 21:59:53 +0530

    deque code add

--- src/component/NoteCard/NoteCard.js
+++ src/component/NoteCard/NoteCard.js
@@ -1,17 +1,22 @@
 import React from "react";
 import "./NoteCard.css";
 
-function NoteCard({ title, description, category, emoji, onDelete}) {
+function NoteCard({ title, description, category, emoji, onDelete }) {
   return (
     <div className="note-card">
-      <div className="note-card-emoji">{emoji}</div>
-      <div>
-        <h3 className="note-card-title">{title}</h3>
-        <p className="note-card-description">{description}</p>
-        <span className="note-card-category">{category}</span>
-        <button className="delete-button" onClick={onDelete}>
-        Delete
-      </button>
+      <div className="useflex">
+        <div>
+          <div className="note-card-emoji margin">{emoji}</div>
+
+          <h3 className="note-card-title margin">{title}</h3>
+          <p className="note-card-description margin">{description}</p>
+          <span className="note-card-category margin">{category}</span>
+        </div>
+        <div className="button-card">
+          <button className="delete-button" onClick={onDelete}>
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
